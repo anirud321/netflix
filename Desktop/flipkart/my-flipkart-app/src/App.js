a601e79b08263e4dc5935b1f7a5c466e9a7b2e96
anirudh p <anirudh.p.dq@gmail.com>
2022-05-09 12:24:30 -0400

    flipkard

---
+++ Desktop/flipkart/my-flipkart-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import  './App.css'
+function App() {
+  return (
+    <div className='main'>
+      <nav className='navmain'>
+        <div>
+            <img src='flipkart.webp'></img>
+          <input type="search " placeholder='Search for products, brands and more'></input>
+          <button>Login</button>
+        </div>
+        <div>become a seller</div>
+        <div>more</div>
+        <div>card</div>
+      </nav>
+    </div>
+  )
+}
+
+export default App
